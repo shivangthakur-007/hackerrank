ec5b191c0e937687d091d8b5892a775cade3465a
Dance <thakurshivang579@gmail.com>
2024-08-25 11:10:10 +0530

    data structure and algorithms

--- leatcode/a.js
+++ leatcode/a.js
@@ -13,11 +13,98 @@ var map = function (arr, fn) {
 // The fn function takes one or two arguments:
 
 var filter = function (arr, fn) {
-    let filteredArr = []
-    for (let i = 0; i < arr.length; i++) {
-        if (fn(arr[i], i)) {
-            filteredArr.push(arr[i])
-        }
+  let filteredArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      filteredArr.push(arr[i]);
     }
-    return filteredArr
+  }
+  return filteredArr;
+};
+
+function f(x) {
+  x = "x-" + x;
+  return function (y) {
+    y = "y-" + x;
+    return function (z) {
+      return "z-" + y;
+    };
+  };
 }
+
+let g = f("a")("b")("c");
+// console.log(g);
+
+function BinarySearch(arr, target) {
+  let leftindex = 0;
+  let rightindex = arr.length - 1;
+  while (leftindex <= rightindex) {
+    let middleindex = Math.floor((leftindex + rightindex) / 2);
+    if (target === arr[middleindex]) {
+      return middleindex;
+    }
+    if (target < arr[middleindex]) {
+      rightindex = middleindex - 1;
+    } else {
+      leftindex = middleindex + 1;
+    }
+  }
+  return -1;
+}
+
+// console.log(BinarySearch([10, 20, 21, 30, 40], 40));
+
+function RecursiveBinarySearch(arr, target) {
+  return Search(arr, target, 0, arr.length - 1);
+}
+
+function Search(arr, target, leftindex, rightindex) {
+  if (leftindex > rightindex) {
+    return -1;
+  }
+  let middleindex = Math.floor((leftindex + rightindex) / 2);
+  if (target === arr[middleindex]) {
+    return middleindex;
+  }
+  if (target < arr[middleindex]) {
+    return Search(arr, target, leftindex, middleindex - 1);
+  } else {
+    return Search(arr, target, middleindex + 1, rightindex);
+  }
+}
+
+// console.log(RecursiveBinarySearch([10, 20, 21, 30, 40], 40));
+
+function BubbleSort(arr) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let element = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = element;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+}
+let arr = [1, -6, 2, -10];
+BubbleSort(arr);
+// console.log(arr);
+
+// Insertion Sort
+
+function InsertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let numberToinsert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numberToinsert) {
+      arr[j+1]= arr[j]
+      j= j-1
+    }
+    arr[j+1] = numberToinsert
+  }
+}
+InsertionSort(arr)
+console.log(arr)
