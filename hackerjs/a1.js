3f05ada2afd160f05e5c97bd831800a08dbaf7ec
Dance <thakurshivang579@gmail.com>
2024-09-11 09:00:34 +0530

    merge sort

--- hackerjs/a1.js
+++ hackerjs/a1.js
@@ -1,5 +1,31 @@
 function birthdayCakeCandles(candles) {
   // Write your code here
-  
+  // let;
 }
 birthdayCakeCandles([3, 3, 1, 2]);
+
+function MergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+
+  return merge(MergeSort(left), MergeSort(right));
+}
+
+function merge(left, right) {
+  const sortarr = [];
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      sortarr.push(left.shift());
+    } else {
+      sortarr.push(right.shift());
+    }
+  }
+  return [...sortarr, ...left, ...right];
+}
+const arr = [8, 20, -2, -6, 4];
+
+console.log(MergeSort(arr));
